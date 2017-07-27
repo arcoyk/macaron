08443aa7f6a12373a94d2b2dbbf3e8dbc65f07a0
yui <yui@racoon.local>
2017-07-27 19:59:08 +0900

    fix

--- play.py
+++ play.py
@@ -3,17 +3,17 @@ import os
 import sys
 import random
 
-root = '/a/lot/of/songs/you/like/'
+root = '/users/yui/desktop/movs/'
 playlist = list()
 args = sys.argv[1:]
 for d in os.listdir(root):
     for arg in args:
         if d.lower().find(arg) != -1:
             playlist.append(root + d)
-if playlist.size() < 1:
+if not playlist:
     print("I'm hungry (songs not found)")
 random.shuffle(playlist)
 cmd = ' && afplay '.join(playlist)
 for music in playlist:
-    print('🎵Now Playing...', music, "in", playlist.size(), "songs in instant playlist...")
+    print('🎵  Now Playing...', music, "in", len(playlist), "songs in instant playlist...")
     call(['afplay', music])
