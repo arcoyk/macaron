d38aae7f165853a6ddce5305d4b6c3766a8931cd
yui <yuikita21@gmail.com>
2017-10-10 18:17:39 +0900

    config

--- play.py
+++ play.py
@@ -2,9 +2,12 @@ from subprocess import call
 import os
 import sys
 import random
+import json
 
-root = '/users/yui/desktop/movs/'
-args = sys.argv[1:]
+def load_config(config_path):
+  with open(config_path) as f:
+    config = json.load(f)
+    return config
 
 def titles(root):
   return list(map(lambda x:root + x, os.listdir(root)))
@@ -22,27 +25,15 @@ def search(tars, posi=[], nega=[]):
       rst.append(tar)
   return rst
 
-def parse_args(args):
-  likes = list()
-  dislikes = list()
-  for arg in args:
-    # Remove titles start with '-' (ex. '-bad_song')
-    if arg[0] == '-':
-      dislikes.append(arg[1:])
-    else:
-      likes.append(arg)
-  return likes, dislikes
-
-def instant_playlist(args):
-  likes, dislikes = parse_args(args)
-  playlist = titles(root)
-  playlist = search(playlist, posi=likes, nega=dislikes) 
-  loop = True
-  mix = True
-  if 'noloop' in args:
-    loop = False
-  if 'nomix' in args:
-    mix = False
+def instant_playlist(params):
+  # config overrides default
+  # option overrides config
+  mix = params['mix']
+  loop = params['loop']
+  allpath = titles(params['music_dir'])
+  likes = params['likes']
+  dislikes = params['dislikes']
+  playlist = search(allpath, posi=likes, nega=dislikes) 
   return mix, loop, playlist
 
 def keywords():
@@ -88,5 +79,72 @@ def play(playlist, mix=False, loop=False):
   if loop:
     play(playlist, mix, loop)
 
-mix, loop, playlist = instant_playlist(args)
-play(playlist, mix, loop)
+def is_option(arg):
+  return arg[:2] == '--'
+
+def update_rst(rst, option, params):
+  # If option expects a list set params as value
+  # Otherwise, add params to default
+  # and set the value True
+  p_options = ['likes', 'dislikes']
+  default = 'likes'
+  if option in p_options:
+    rst[option] = params
+  else:
+    rst[default] += params
+    rst[option] = True
+  return rst
+
+def parse_args(args):
+  rst = {}
+  # Necessary options
+  rst['likes'] = []
+  rst['dislikes'] = []
+  option = 'likes'
+  params = []
+  for i in range(len(args)):
+    arg = args[i]
+    if is_option(arg):
+      rst = update_rst(rst, option, params)
+      option = arg[2:]
+      params = []
+    else:
+      params.append(arg)
+  rst = update_rst(rst, option, params)
+  return rst
+
+def merge(config, option):
+  # Add or override config by option
+  if 'nomix' in option.keys():
+    config['mix'] = False
+  if 'mix' in option.keys():
+    config['mix'] = True
+  if 'noloop' in option.keys():
+    config['loop'] = False
+  if 'loop' in option.keys():
+    config['loop'] = True
+  # Transfer necessary options
+  config['likes'] = option['likes']
+  config['dislikes'] = option['dislikes']
+  # Unfold alias
+  for key in config['alias']:
+    if key in config['likes']:
+      config['likes'] += config['alias'][key]
+    if key in config['dislikes']:
+      config['dislikes'] += config['alias'][key]
+  config.pop('alias', None)
+  return config
+
+config = load_config('config.json')
+# args = sys.argv[1:]
+args = ['python', 'banana', '--loop', 'utada', '--nomix', '--dislikes', 'Utada']
+# Consider args as a manipulation for default params
+# to enable easy args (e.g. '--nomix', not '--mix=false')
+option = parse_args(args[1:])
+params = merge(config, option)
+mix, loop, playlist = instant_playlist(params)
+print(playlist)
+# play(playlist, mix, loop)
+
+
+
